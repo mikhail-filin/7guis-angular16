417b51df305b25da51ad16ab47fda76d6fc2135e
Mikhail Filin <mikhailf.codes@gmail.com>
2023-05-30 20:33:29 +0700

    Remove default boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,11 +17,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('7guis-angular16');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('7guis-angular16 app is running!');
-  });
 });
