48b62d875fc23f9188fef63c7d4adc62af643d37
Mikhail Filin <mikhailf.codes@gmail.com>
2023-06-24 13:47:45 +0700

    Add timer implementation

--- src/app/page-features/components/guis/timer/timer.component.ts
+++ src/app/page-features/components/guis/timer/timer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { TIMER_RANGE_MAX, TIMER_RANGE_MIN, UPDATE_STEP_MS } from './timer.const';
 
 @Component({
   selector: 'guis-timer',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent {
+  public progress = signal(0);
+  public duration = signal(0);
+  public progressPercentage = computed(() => {
+    if (!this.progress() && !this.duration()) return 0;
+    return Math.min(this.progress() / this.duration() * 100, 100);
+  });
 
+  protected readonly TIMER_RANGE_MAX = TIMER_RANGE_MAX;
+  protected readonly TIMER_RANGE_MIN = TIMER_RANGE_MIN;
+  private timerId: NodeJS.Timer | undefined;
+
+  public resetTimer(): void {
+    this.progress.set(0);
+
+    this.resetInterval();
+    this.startTimer();
+  }
+
+  public durationChange(event: Event): void {
+    const rangeEl = event.target as HTMLInputElement;
+    this.duration.set(+rangeEl.value);
+    this.startTimer();
+  }
+
+  private startTimer(): void {
+    if (this.timerId) return;
+
+    this.timerId = setInterval(() => {
+      if (this.progress() >= this.duration()) {
+        this.resetInterval();
+        return;
+      }
+      this.progress.update((progress) => +(progress + UPDATE_STEP_MS / 1000).toFixed(1));
+    }, UPDATE_STEP_MS);
+  }
+
+  private resetInterval(): void {
+    clearInterval(this.timerId);
+    this.timerId = undefined;
+  }
 }
---
+++ src/app/page-features/components/guis/timer/timer.const.ts
@@ -0,0 +1,3 @@
+export const TIMER_RANGE_MIN: number = 0;
+export const TIMER_RANGE_MAX: number = 60;
+export const UPDATE_STEP_MS: number = 100;
