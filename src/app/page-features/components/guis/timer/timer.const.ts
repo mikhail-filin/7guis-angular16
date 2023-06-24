c63f7d1380d88d9d8f27675ea2645a9d9980c07b
Mikhail Filin <mikhailf.codes@gmail.com>
2023-06-24 14:16:15 +0700

    Fix linter errors

--- src/app/page-features/components/guis/timer/timer.const.ts
+++ src/app/page-features/components/guis/timer/timer.const.ts
@@ -1,3 +1,3 @@
-export const TIMER_RANGE_MIN: number = 0;
-export const TIMER_RANGE_MAX: number = 60;
-export const UPDATE_STEP_MS: number = 100;
+export const TIMER_RANGE_MIN = 0;
+export const TIMER_RANGE_MAX = 60;
+export const UPDATE_STEP_MS = 100;
