e56c58c7fed79954934fdc569d7402ef3c5f6bca
Mikhail Filin <mikhailf.codes@gmail.com>
2023-07-03 19:05:12 +0700

    Implement CRUD improvements
    Replace set with array; start to save selected index instead of whole name; make prefix filter case insensitive

--- src/app/page-features/components/guis/crud/crud.component.ts
+++ src/app/page-features/components/guis/crud/crud.component.ts
@@ -6,8 +6,8 @@ import { Component, computed, signal, WritableSignal } from '@angular/core';
   styleUrls: ['./crud.component.scss']
 })
 export class CrudComponent {
-  public listNames: WritableSignal<Set<string>> = signal(new Set());
-  public selectedRecord: WritableSignal<string | null> = signal(null);
+  public listNames: WritableSignal<string[]> = signal([]);
+  public selectedIndex: WritableSignal<number | null> = signal(null);
   public firstName: WritableSignal<string | null> = signal(null);
   public lastName: WritableSignal<string | null> = signal(null);
   public filterValue: WritableSignal<string | null> = signal(null);
@@ -17,48 +17,50 @@ export class CrudComponent {
   });
   public filteredNames = computed(() => {
     const filter = this.filterValue();
-    const names = Array.from(this.listNames());
+    const names = this.listNames();
+
     if (!filter) return names;
 
     return names.filter((name) => {
-      const surname = name.split(',')[0];
-      return surname.startsWith(filter);
+      const surname = name.split(',')[0].toLowerCase();
+      return surname.startsWith(filter.toLowerCase());
     });
   });
 
   public createRecord(): void {
     const newName = `${this.lastName()}, ${this.firstName()}`;
-    if (this.listNames().has(newName)) return;
 
-    this.listNames.mutate((set) => {
-      set.add(newName);
+    this.listNames.mutate((names) => {
+      names.push(newName);
     })
     this.resetNameFields();
   }
 
   public updateRecord(): void {
-    const selectedRecord = this.selectedRecord();
+    const selectedIndex = this.selectedIndex();
     const updatedName = `${this.lastName()}, ${this.firstName()}`
-    if (!selectedRecord || this.listNames().has(updatedName)) return;
 
-    this.listNames.mutate((set) => {
-      set.delete(selectedRecord);
-      set.add(updatedName)
+    if (selectedIndex === null) return;
+
+    this.listNames.mutate((names) => {
+      names[selectedIndex] = updatedName;
     });
-    this.selectedRecord.set(null);
+    this.selectedIndex.set(null);
     this.resetNameFields();
   }
 
   public deleteRecord(): void {
-    const selectedRecord = this.selectedRecord();
-    if (!selectedRecord) return;
+    const selectedIndex = this.selectedIndex();
+    if (selectedIndex === null) return;
 
-    this.listNames.mutate((set) => set.delete(selectedRecord));
-    this.selectedRecord.set(null);
+    this.listNames.mutate((names) => {
+      names.splice(selectedIndex, 1);
+    });
+    this.selectedIndex.set(null);
   }
 
-  public selectRecord(name: string): void {
-    this.selectedRecord.set(name);
+  public selectRecord(index: number): void {
+    this.selectedIndex.set(index);
   }
 
   private resetNameFields(): void {
