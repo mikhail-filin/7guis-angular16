06e5ef0360789db75f038d38a43fd617e4f49fa8
Mikhail Filin <mikhailf.codes@gmail.com>
2023-07-12 17:01:40 +0700

    Add circle implementation (without history change)

--- src/app/page-features/components/guis/circle-drawer/circle-drawer.component.ts
+++ src/app/page-features/components/guis/circle-drawer/circle-drawer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal, TemplateRef, ViewChild, ViewContainerRef, WritableSignal } from '@angular/core';
+import { Circle } from './circle.model';
+import { DEFAULT_RADIUS } from './circle-drawer.const';
 
 @Component({
   selector: 'guis-circle-drawer',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./circle-drawer.component.scss']
 })
 export class CircleDrawerComponent {
+  @ViewChild('canvas', { read: ViewContainerRef }) canvas: ViewContainerRef | undefined;
+  @ViewChild('circle') circleTemplate: TemplateRef<unknown> | undefined;
 
+  public circles: WritableSignal<Circle[]> = signal([]);
+  public selectedCircle: WritableSignal<Circle | null> = signal(null);
+
+  public undoAction(): void {
+  }
+
+  public redoAction(): void {
+  }
+
+  public createCircle(event: MouseEvent): void {
+    if (this.selectedCircle()) {
+      this.resetSelection();
+      return;
+    }
+
+    const newCircle: Circle = {
+      cx: event.offsetX,
+      cy: event.offsetY,
+      r: DEFAULT_RADIUS
+    };
+    this.circles.mutate((val: Circle[]) => val.push(newCircle));
+  }
+
+  public showPopup(event: Event, circle: Circle): void {
+    event.preventDefault();
+    this.resetSelection();
+    this.selectedCircle.set(circle);
+  }
+
+  public radiusChange(newRadius: number): void {
+    const selectedCircle = this.selectedCircle();
+    if (!selectedCircle) {
+      return;
+    }
+
+    selectedCircle.r = newRadius;
+  }
+
+  private resetSelection() {
+    this.selectedCircle.set(null);
+  }
 }
